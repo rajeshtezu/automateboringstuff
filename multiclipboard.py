448bead845cc9fa5d3439de59aefcf4f6cb3ef64
Rajesh Kumar <rajeshtezu90@gmail.com>
2017-08-07 12:20:41 +0300

    multiclipboard modified2

--- multiclipboard.py
+++ multiclipboard.py
@@ -14,14 +14,14 @@ mcbShelf = shelve.open('mcb')
 if len(sys.argv) == 3 and sys.argv[1].lower() == 'save':
     mcbShelf[sys.argv[2]] = pyperclip.paste()
 elif len(sys.argv) == 3 and sys.argv[1].lower() == 'delete':
-    del mcbShelf[argv[2]]
+    del mcbShelf[sys.argv[2]]
     #mcbShelf[sys.argv[2]] = pyperclip.paste()
 elif len(sys.argv) == 2:
     # TODO: List keywords and load content.
     if sys.argv[1].lower() == 'list':
         pyperclip.copy(str(list(mcbShelf.keys())))
     elif sys.argv[1].lower() == 'delete':
-        del mcbShelf.keys()
+        del mcbShelf
     elif sys.argv[1] in mcbShelf:
         pyperclip.copy(mcbShelf[sys.argv[1]])
         
