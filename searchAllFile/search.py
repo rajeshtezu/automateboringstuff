6239aeb9d97c1439ce3fafba1267136fc85cdab2
Rajesh Kumar <rajeshtezu90@gmail.com>
2017-08-07 15:20:54 +0300

    file Reading Writing finished

--- searchAllFile/search.py
+++ searchAllFile/search.py
@@ -1,4 +1,7 @@
 # python3
+# 
+# A program that opens all .txt files in a folder and searches for any line that matches a user-supplied 
+# regular expression. The results should be printed to the screen.
 
 import re, glob
 
