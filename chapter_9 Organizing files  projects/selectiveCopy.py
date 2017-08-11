a4438e8cfbf1a6caaae4b04d96c5221a1d5c2f3f
Rajesh Kumar <rajeshtezu90@gmail.com>
2017-08-11 13:28:41 +0300

    look changes

--- chapter_9 Organizing files  projects/selectiveCopy.py
+++ chapter_9 Organizing files  projects/selectiveCopy.py
@@ -1,6 +1,8 @@
 #! Python3
 #a program that walks through a folder tree and searches for files with a certain file extension (such as .pdf or 
 #.jpg). Copy these files from whatever location they are in to a new folder.
+#
+# Usage: python selectiveCopy
 
 import os, shutil
 
