fb4d6fdcc69a16926fb8f62643bf11346f99e511
Rajesh Kumar <rajeshtezu90@gmail.com>
2017-08-30 15:16:00 +0300

    Validating email

--- chapter_16 Sending Email and Text Messages Projects/sendMail.py
+++ chapter_16 Sending Email and Text Messages Projects/sendMail.py
@@ -1,48 +1,73 @@
 #! /usr/bin/python
 # Sending mail from Gmail account.
 
-import smtplib, sys
+import smtplib, sys, getpass, re
 
-smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
-isConnected = smtpObj.ehlo()
-
-# Check connection establishment
-if isConnected[0] == 250:
-    isServerReady = smtpObj.starttls()
+def main():
+    smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
+    isConnected = smtpObj.ehlo()
     
-    # Check if server is ready
-    if isServerReady[0] == 220:
-        yourMail = input('Enter your gmail username: ')
-        yourPass = input('Enter your gmail password: ')
-        
-        # Login to your account
-        try:
-            smtpObj.login(yourMail, yourPass)
-            recipientMail = input('To: ')
-            subject = input('Subject: ')
-            subject = 'Subject: ' + subject
-            body = input('Mail Text: ')
-            body = subject + '\n' + body
-            
-            # Send mail
-            success = smtpObj.sendmail(yourMail, recipientMail, body)
-            
-            if success == {}:
-                print('Mail sent successfully to ' + recipientMail)
+    # Check connection establishment
+    if isConnected[0] == 250:
+        isServerReady = smtpObj.starttls()
+    
+        # Check if server is ready
+        if isServerReady[0] == 220:
+            yourMail = input('Enter your gmail username: ')
+            while not validateEmail(yourMail):
+                print('Incorrect mail.')
+                yourMail = input('Pleae enter valid gmail address: ')
+            yourPass = getpass.getpass(prompt='Password: ', stream=None)
+    
+            # Login to your account
+            try:
+                smtpObj.login(yourMail, yourPass)
+                recipientMail = input('To: ')
+                while not validateEmail(recipientMail):
+                    print('Incorrect address.')
+                    recipientMail = input('Enter valid address: ')
                 
-            # Logout from your account
-            logout = smtpObj.quit()
-            if logout[0] == 221:
-                print('Logged out from your account ' + yourMail)
-                
-        except smtplib.SMTPAuthenticationError:
-            print('Check your username and password.')
+                subject = input('Subject: ')
+                subject = 'Subject: ' + subject
+                body = input('Mail Text: ')
+                body = subject + '\n' + body
+    
+                # Send mail
+                success = smtpObj.sendmail(yourMail, recipientMail, body)
+    
+                if success == {}:
+                    print('Mail sent successfully to ' + recipientMail)
+    
+                # Logout from your account
+                logout = smtpObj.quit()
+                if logout[0] == 221:
+                    print('Logged out from your account ' + yourMail)
+    
+            except smtplib.SMTPAuthenticationError:
+                print('Check your username and password.')
+    
+        else:
+            sys.exit('Server is not ready.')
     
     else:
-        sys.exit('Server is not ready.')
+        sys.exit('Could not connect to smtp server.')
 
-else:
-    sys.exit('Could not connect to smtp server.')
 
     
+    
+def validateEmail(mail):
+    emailRegex = re.compile(r'''(
+     [a-zA-Z0-9._%+-]+      # username
+     @                      # @ symbol
+     [a-zA-Z0-9.-]+         # domain name
+     (\.[a-zA-Z]{2,4})      # dot-something
+     )''', re.VERBOSE)
+    
+    if emailRegex.search(mail):
+        return True
+    else:
+        return False
 
+    
+if __name__ == '__main__':
+    main()
