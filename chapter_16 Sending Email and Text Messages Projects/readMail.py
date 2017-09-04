962a246a55811817d8a66f0117bf65d094458b00
Rajesh Kumar <rajeshtezu90@gmail.com>
2017-09-04 09:21:08 +0300

    readMail

--- chapter_16 Sending Email and Text Messages Projects/readMail.py
+++ chapter_16 Sending Email and Text Messages Projects/readMail.py
@@ -1,12 +1,13 @@
 #! /usr/bin/python3
 # read Gmail inbox
-
 import pyzmail
 import imapclient
 import pprint
 
-username = 'myMail@gmail.com'
-password = 'MyPassword'
+#imaplib._MAXLINE = 10000000
+
+username = 'myEmail@gmail.com'
+password = 'myPassword'
 
 imapObj = imapclient.IMAPClient('imap.gmail.com', ssl=True)
 imapObj.login(username, password)
@@ -16,10 +17,24 @@ folderList = imapObj.list_folders()
 #pprint.pprint(folderList)
 imapObj.select_folder('INBOX', readonly=True)
 
+UIDs = imapObj.search(['ALL'])
+#pprint.pprint(UIDs)
+    
+rawMessages = imapObj.fetch(UIDs[0], ['BODY[]'])
+#pprint.pprint(rawMessages)
 
+message = pyzmail.PyzMessage.factory(rawMessages[7][b'BODY[]'])
+pprint.pprint(message.get_subject())
+pprint.pprint(message.get_addresses('from'))
+pprint.pprint(message.get_addresses('to'))
+pprint.pprint(message.get_addresses('cc'))
+pprint.pprint(message.get_addresses('bcc'))
 
+bodyText = message.text_part.get_payload().decode(message.text_part.charset)
+#pprint.pprint(bodyText)
+print(bodyText)
 
-
+imapObj.logout()
 
 
 
